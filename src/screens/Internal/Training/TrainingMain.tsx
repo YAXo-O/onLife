5e5d28bb6c1c63b78676806852154882f07a9b37
Yermakov Alex <aermakov@sgsdt.com>
2022-10-07 13:01:55 +0300

    feature/service-redesign: waring on logout (fixed not started exercises not being taken into account)

--- src/screens/Internal/Training/TrainingMain.tsx
+++ src/screens/Internal/Training/TrainingMain.tsx
@@ -18,15 +18,19 @@ interface LogOutProps {
 }
 
 const LogOut: React.FC<LogOutProps> = (props: LogOutProps) => {
-	const { logOut } = withUser();
+	const { logOut, user } = withUser();
 	const currentTraining = useSelector((state: IState) => state.training.item);
 
 	const handler = () => {
 		const day = currentTraining?.training?.days.find(day => day.programDayId === currentTraining?.day);
+		const pDay = user?.trainingProgram?.days.find(day => day.id === currentTraining?.day);
+		const pList = pDay?.exercises?.map(item => item.rounds).flat() ?? [];
 		const list = day?.exercises?.map(item => item.rounds).flat() ?? [];
 		const count = list.reduce((counter, cur) => Boolean(cur.time) ? counter + 1 : counter, 0);
 
-		if (count !== list.length && count > 0) {
+		// pList.length !== list.length - some exercises of the day haven't been started yet
+		// count !== list.length && count > 0 - some exercises have been started but not finished
+		if (pList.length !== list.length && list.length > 0 || count !== list.length && count > 0) {
 			Alert.alert(
 				'Смена пользователя',
 				'Все данные о незавершённой тренировке будут утеряны. Вы уверены, что хотите продолжить?',
